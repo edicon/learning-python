31ac55a0b1775d69f92818f2b407a5f139fc666b
mglish <dev.mglish@gmail.com>
2021-11-02 17:26:45 +0900

    test: copilot

---
+++ convert_image_to_pencil.py
@@ -0,0 +1,5 @@
+# Convert any image to a pencil sketch — with just 2 lines of code!
+# -https://medium.com/analytics-vidhya/image-to-pencil-sketch-in-just-2-lines-of-code-292de57be483
+# $ pip install sketchify
+from sketchify import sketch
+sketch.normalsketch('./elephant.jpeg', './saveImage', 'elephant')
